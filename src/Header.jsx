a55fcf201319f1a456540d9f5e49d4034799bb02
tienpham <tpha768@gmail.com>
2026-01-10 22:32:55 +0700

    add: dynamic value

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+const randomIndex = (num) => Math.floor(Math.random() * num);
+const yearOfExperiences = ["Fresher", "Middle", "Junior", "Senior"];
+const Header = () => {
+  const yof = yearOfExperiences[randomIndex(yearOfExperiences.length)];
+  return (
+    <header>
+      <h1>Welcome to My Website</h1>
+      <p>I have {yof} in web development</p>
+    </header>
+  );
+};
+export default Header;
