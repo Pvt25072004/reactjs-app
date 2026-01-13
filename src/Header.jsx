1819c2028b00756dcdd15b59d0a094c04c512d4b
tienpham <tpha768@gmail.com>
2026-01-13 23:02:43 +0700

    add: setting attributes dynamic and loading image files

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,5 @@
+import { userData, adminData } from "./constants/userData.js";
+import imageProgramming from "../public/images/programming.avif";
 const yearOfExperiences = ["Fresher", "Middle", "Junior", "Senior"];
 const randomIndex = (num) => Math.floor(Math.random() * num);
 const Header = () => {
@@ -6,6 +8,18 @@ const Header = () => {
     <header>
       <h1>Welcome to My Website</h1>
       <p>I have {yoe} in web development</p>
+      <h3>My info about me</h3>
+      <ul>
+        <li>Name: {userData.name}</li>
+        <li>Age: {userData.age}</li>
+        <li>Email: {userData.email}</li>
+      </ul>
+      <img src={imageProgramming} alt="programming" width="300" />
+      <h3>Admin Info</h3>
+      <ul>
+        <li>Name: {adminData.name}</li>
+        <li>Password: {adminData.password}</li>
+      </ul>
     </header>
   );
 };
---
+++ src/constants/userData.js
@@ -0,0 +1,21 @@
+// export const userData = {
+//   name: "John Doe",
+//   age: 30,
+//   email: "John.Doe@example.com",
+// };
+
+// export const adminData = {
+//   name: "Admin User",
+//   password: "admin123",
+// };
+const userData = {
+  name: "John Doe",
+  age: 30,
+  email: "John.Doe@example.com",
+};
+
+const adminData = {
+  name: "Admin User",
+  password: "admin123",
+};
+export { userData, adminData };
