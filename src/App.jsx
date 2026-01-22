0eacb8b7afd25b7a7180dc2b53747578bad512b3
tienpham <tpha768@gmail.com>
2026-01-22 22:27:24 +0700

    add: do homework reacting to events

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,23 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./Header.jsx";
 import TabButton from "./components/tabButton.jsx";
+import FormUserLogin from "./hooks/formData.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
   const onClickHandle = (n) => () => setText(n);
   const tabText = {
     1: "Home button clicked",
     2: "Product button clicked",
     3: "Cart button clicked",
+  };
+  const onHandleInput = (e) => {
+    e.preventDefault();
+    console.log(username, password);
   };
   return (
     <>
@@ -54,6 +62,28 @@ function App() {
         </menu>
       </section>
       <Header />
+
+      <form action="">
+        <FormUserLogin
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <FormUserLogin
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button onClick={onHandleInput}>Login</button>
+      </form>
     </>
   );
 }
